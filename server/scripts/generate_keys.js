9eb3cbd9588d61fd0b5651e2268544879a31aede
Carles Gonzalvo <cgonzalvo@newrelic.com>
2023-04-16 17:26:34 +0200

    fix generate keys bug - ignore compression byte for computing public address

--- server/scripts/generate_keys.js
+++ server/scripts/generate_keys.js
@@ -4,7 +4,7 @@ const { keccak256 } = require("ethereum-cryptography/keccak");
 
 const privateKey = secp256k1.utils.randomPrivateKey();
 const publicKey = secp256k1.getPublicKey(privateKey);
-const publicAddress = keccak256(publicKey).slice(12);
+const publicAddress = keccak256(publicKey.slice(1)).slice(12);
 
 console.log(`Private key: 0x${toHex(privateKey)}`);
 console.log(`Public key: 0x${toHex(publicKey)}`);
